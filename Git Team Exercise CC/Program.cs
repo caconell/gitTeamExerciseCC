d243e9f927a50b6108bc135dc47263989e562d4e
caconell <caconell88@gmail.com>
2021-05-21 14:38:57 -0400

    Initial Commit

---
+++ Git Team Exercise CC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Team_Exercise_CC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
